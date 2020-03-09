5711d7db62c8e0b427e1cd0573fce6b26036fdbf
Matthew Bunday <mkbunday@gmail.com>
2020-03-09 15:35:20 -0400

    work on login

--- components/AuthGate.js
+++ components/AuthGate.js
@@ -2,22 +2,53 @@ import * as React from 'react';
 import AuthToken from '~/lib/AuthToken';
 import { TextInput, Picker, StyleSheet, SafeAreaView } from 'react-native';
 import SfTextInput from '~/components/SfTextInput';
+import { API_URL } from '~/lib/constants';
+import { debounce } from 'lodash';
 
-const AuthGate = props => {
-  const [token, setToken] = React.useState(AuthToken.get());
-  const [username, setUsername] = React.useState('');
+class AuthGate extends React.Component {
+  constructor(props) {
+    this.state = {
+      fetchingNameAvailable: false,
+      nameAvailable: null,
+      token: AuthToken.get(),
+      username: '',
+    };
+    super(props);
+  }
 
-  return !token ? (
-    <SafeAreaView>
-      <SfTextInput
-        placeholder="User name"
-        onChangeText={setUsername}
-        value={username}
-      />
-    </SafeAreaView>
-  ) : (
-    props.children
-  );
-};
+  fetchNameAvailable = debounce(username => {
+    this.setState({ fetchingNameAvailable: true });
+    fetch(`${API_URL}available/${username}`)
+      .then(resp => resp.json())
+      .then(available => {
+        this.setState({
+          nameAvailable: available,
+          fetchingNameAvailable: false,
+        });
+      });
+  });
+
+  handleUsername = username => {
+    this.setState({ username });
+    this.fetchNameAvailable(username);
+  };
+
+  render() {
+    const { token, username, fetchingNameAvailable } = this.state;
+
+    return !token ? (
+      <SafeAreaView>
+        <SfTextInput
+          placeholder="User name"
+          onChangeText={handleUsername}
+          value={username}
+        />
+        {fetchingNameAvailable ? 'Loading...' : nameAvailable}
+      </SafeAreaView>
+    ) : (
+      this.props.children
+    );
+  }
+}
 
 export default AuthGate;
