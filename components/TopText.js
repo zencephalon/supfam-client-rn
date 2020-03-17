adabd84f09b45b4fe54cc2e27ebc58b09ba32ce0
Matthew Bunday <mkbunday@gmail.com>
2020-03-17 18:14:36 -0400

    Try out some icons

--- components/TopText.js
+++ components/TopText.js
@@ -6,17 +6,24 @@ import { View, Text } from 'react-native';
 
 import { textSecondary, textTertiary } from '~/constants/Colors';
 
+import { Ionicons, MaterialCommunityIcons } from '@expo/vector-icons';
+import { nord10 } from '~/constants/Colors';
+
 export default function TopText({ displayName, locationState, lastUpdate }) {
   return (
     <View
       style={{
         flexDirection: 'row',
         // justifyContent: "space-between",
-        // alignItems: "flex-end",
+        alignItems: 'flex-end',
         flexGrow: 1,
       }}
     >
-      <Text style={{ width: '25%', color: textSecondary }}>{displayName}</Text>
+      <Text style={{ color: textSecondary }}>{displayName}</Text>
+      <MaterialCommunityIcons name="battery-40" size={14} color="#BBB" />
+      <MaterialCommunityIcons name="wifi-strength-2" size={14} color="#BBB" />
+      {/* <MaterialCommunityIcons name="signal-3g" size={14} color="#BBB" /> */}
+
       {/* <Text
         style={{
           width: "50%",
