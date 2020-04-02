5106f233bcc00410c93f3fba9a2f96e5effad162
Matthew Bunday <mkbunday@gmail.com>
2020-04-02 16:08:14 -0400

    Clearer status button?

--- c/StatusButton.js
+++ c/StatusButton.js
@@ -6,7 +6,10 @@ import SfText from '~/c/SfText';
 
 const colorLabels = ['Away', 'Busy', 'Free', 'Open'];
 
+import useLight from '~/hooks/useLight';
+
 const StatusButton = ({ color, setColor, selected }) => {
+  const { foregrounds, backgrounds } = useLight();
   return (
     <TouchableOpacity
       style={{
@@ -15,7 +18,13 @@ const StatusButton = ({ color, setColor, selected }) => {
       }}
       onPress={() => setColor(color)}
     >
-      <SfText style={{ fontWeight: selected ? '700' : '400', fontSize: 14 }}>
+      <SfText
+        style={{
+          fontWeight: selected ? '700' : '400',
+          fontSize: 14,
+          color: selected ? backgrounds[0] : foregrounds[0],
+        }}
+      >
         {colorLabels[color]}
       </SfText>
     </TouchableOpacity>
