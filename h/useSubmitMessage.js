82920eceda6392536134da078d2ca0879ee7435c
Matthew Bunday <mkbunday@gmail.com>
2020-05-13 11:55:42 -0400

    Extract MessageQueue logic

--- h/useSubmitMessage.js
+++ h/useSubmitMessage.js
@@ -1,9 +1,8 @@
 import React from 'react';
 
-import { sendMessage } from '~/apis/api';
 import sendInstant from '~/lib/sendInstant';
 
-import Cable from '~/lib/Cable';
+import MessageQueue from '~/lib/MessageQueue';
 
 export default function useSubmitMessage(text, conversationId, meProfileId) {
   return React.useCallback(async () => {
@@ -11,6 +10,9 @@ export default function useSubmitMessage(text, conversationId, meProfileId) {
       return;
     }
     sendInstant(conversationId, text);
-    Cable.sendMessage(meProfileId, conversationId, { message: text, type: 0 });
+    MessageQueue.sendMessage(meProfileId, conversationId, {
+      message: text,
+      type: 0,
+    });
   }, [text, conversationId, meProfileId]);
 }
