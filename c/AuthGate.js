e67d6cf3adf3582c4b6b299e7a42b4804b3ac60d
Alan Mooiman <amooiman@gmail.com>
2020-05-23 17:15:53 -0400

    Better styling of auth gate

--- c/AuthGate.js
+++ c/AuthGate.js
@@ -1,17 +1,15 @@
 import * as React from 'react';
-import { StyleSheet } from 'react-native';
+import { StyleSheet, Image, View } from 'react-native';
 import { connect } from 'react-redux';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 
 import SfText from '~/c/SfText';
 import SfContainer from '~/c/SfContainer';
-import RegistrationForm from '~/c/RegistrationForm';
 import LoginForm from '~/c/LoginForm';
 import SfButton from '~/c/SfButton';
 
 import CheckInviteFlow from '~/c/CheckInviteFlow';
-import VerifyCodeFlow from '~/c/VerifyCodeFlow';
 
 import { FREE, OPEN, AWAY } from '~/constants/Colors';
 
@@ -21,21 +19,26 @@ const Stack = createStackNavigator();
 
 function Welcome(props) {
   return (
-    <React.Fragment>
-      {/* <SfText style={styles.welcomeText}>Supfam</SfText> */}
-      {/* <Image source={require('../assets/images/icon.png')} /> */}
-      <SfButton
-        color={OPEN}
-        title="Login"
-        onPress={()=>props.navigation.navigate('Login')}
-      />
-      <SfButton
-        color={FREE}
-        title="Register"
-        onPress={() => props.navigation.navigate('Register')}
-      />
-      <SfButton color={AWAY} title="Download Update" onPress={downloadUpdate} />
-    </React.Fragment>
+    <View style={styles.welcomeView}>
+      <Image source={require('../assets/images/icon.png')} />
+      <View style={{ width: '100%' }}>
+        <SfButton
+          color={OPEN}
+          title="Login"
+          onPress={() => props.navigation.navigate('Login')}
+        />
+        <SfButton
+          color={FREE}
+          title="Register"
+          onPress={() => props.navigation.navigate('Register')}
+        />
+        <SfButton
+          color={AWAY}
+          title="Download Update"
+          onPress={downloadUpdate}
+        />
+      </View>
+    </View>
   );
 }
 
@@ -44,24 +47,27 @@ const AuthGate = function (props) {
     return props.children;
   }
   return (
-    // <SfContainer>
-      <NavigationContainer>
-        <Stack.Navigator>
-          <Stack.Screen name="Home" component={Welcome} options={{ title: 'SupFam' }}/>
-          <Stack.Screen name="Login" component={LoginForm}/>
-          <Stack.Screen name="Register" component={CheckInviteFlow}/>
-        </Stack.Navigator>
-      </NavigationContainer>
-    //* </SfContainer> */}
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          name="Home"
+          component={Welcome}
+          options={{ title: 'SupFam', headerShown: false}}
+        />
+        <Stack.Screen name="Login" component={LoginForm} />
+        <Stack.Screen name="Register" component={CheckInviteFlow} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 };
 
 const styles = StyleSheet.create({
-  welcomeText: {
-    marginTop: 12,
-    marginBottom: 18,
-    fontSize: 32,
-    textAlign: 'center',
+  welcomeView: {
+    flex: 1,
+    flexDirection: 'column',
+    justifyContent: 'space-around',
+    alignItems: 'center',
+    backgroundColor: '#000',
   },
 });
 
