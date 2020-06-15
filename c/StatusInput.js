642743dc69410e3566490b1a6fd0ed8ac1679d3a
Matthew Bunday <mkbunday@gmail.com>
2020-06-14 22:18:12 -0400

    Exquisite multiline status text handling

--- c/StatusInput.js
+++ c/StatusInput.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { StyleSheet, View } from 'react-native';
 
 import SfTextInput from './SfTextInput';
+import SfText from './SfText';
 import { MaterialCommunityIcons } from '@expo/vector-icons';
 import { OPEN } from '~/constants/Colors';
 import statusColors from '~/constants/statusColors';
@@ -11,6 +12,44 @@ import { TouchableOpacity } from 'react-native-gesture-handler';
 import { ProfileIconFromProfile } from '~/c/ProfileIcon';
 
 import useLight from '~/h/useLight';
+
+function MagicInput({ value, onChangeText, statusMessage }) {
+  const [focused, setFocused] = React.useState(false);
+  const { backgrounds } = useLight();
+
+  return (
+    <>
+      {!focused && (
+        <View
+          style={{
+            ...styles.statusInput,
+            flexGrow: 1,
+            flexShrink: 1,
+            backgroundColor: backgrounds[1],
+            borderRadius: 10,
+          }}
+        >
+          <SfText onPress={() => setFocused(true)} numberOfLines={1}>
+            {statusMessage}
+          </SfText>
+        </View>
+      )}
+      {focused && (
+        <SfTextInput
+          placeholder={focused ? '' : statusMessage || 'Loading...'}
+          value={value}
+          autoFocus={true}
+          onChangeText={onChangeText}
+          // onSubmitEditing={postMessage}
+          textInputStyle={styles.statusInput}
+          style={{ flexGrow: 1, flexShrink: 1 }}
+          multiline={true}
+          onBlur={() => setFocused(false)}
+        />
+      )}
+    </>
+  );
+}
 
 export default function StatusInput({
   profile,
@@ -31,25 +70,29 @@ export default function StatusInput({
         borderTopColor: backgrounds[1],
         borderTopWidth: 1,
         paddingTop: 8,
-        alignItems: 'flex-end',
+        alignItems: 'center',
       }}
     >
-      <ProfileIconFromProfile profile={profile} size={48} />
-      <SfTextInput
-        placeholder={statusMe?.message || 'Loading...'}
+      <View
+        style={{
+          height: '100%',
+          flexDirection: 'column-reverse',
+        }}
+      >
+        <ProfileIconFromProfile profile={profile} size={48} />
+      </View>
+      <MagicInput
         value={message}
         onChangeText={setMessage}
-        // onSubmitEditing={postMessage}
-        textInputStyle={styles.statusInput}
-        style={{ flexGrow: 1, flexShrink: 1 }}
-        multiline={true}
+        statusMessage={statusMe?.message}
       />
       {!!message && (
         <TouchableOpacity
           onPress={postMessage}
           style={{
-            alignSelf: 'flex-start',
             marginLeft: 4,
+            justifyContent: 'flex-end',
+            flexGrow: 1,
           }}
         >
           <MaterialCommunityIcons
@@ -65,10 +108,12 @@ export default function StatusInput({
 
 const styles = StyleSheet.create({
   statusInput: {
-    padding: 12,
     fontSize: 24,
     borderRadius: 10,
     borderWidth: 0,
+    paddingRight: 8,
+    paddingLeft: 8,
+    paddingTop: 4,
     paddingBottom: 4,
     marginLeft: 8,
   },
