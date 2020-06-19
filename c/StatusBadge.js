ecb5657fc258be9851917a5441ff27dd3b40062b
Matthew Bunday <mkbunday@gmail.com>
2020-06-19 14:19:51 -0400

    Clean up

--- c/StatusBadge.js
+++ c/StatusBadge.js
@@ -1,7 +1,5 @@
 import * as React from 'react';
-import {
-  MaterialCommunityIcons,
-} from '@expo/vector-icons';
+import { MaterialCommunityIcons } from '@expo/vector-icons';
 import statusColors from '~/constants/statusColors';
 
 import useInterval from '@use-it/interval';
@@ -12,11 +10,11 @@ import { View } from 'react-native';
 const colorIcons = [
   'close-circle',
   'pause-circle',
-  'play-circle',
-  'plus-circle', // flash-circle, star-circle, heart-circle
+  'play-circle', // record-circle
+  'chevron-up-circle', // flash-circle, star-circle, heart-circle, plus-circle
 ];
 
-function getIcon(statusColor, lastSeen) {
+function getIcon(lastSeen) {
   const diff = new Date() - new Date(lastSeen);
   const onlineNow = diff < 30 * 1000;
   return onlineNow ? 'eye-circle' : null;
@@ -25,12 +23,12 @@ function getIcon(statusColor, lastSeen) {
 export default function StatusBadge({ statusColor, style, size, lastSeen }) {
   const diff = new Date() - new Date(lastSeen);
   const onlineNow = diff < 30 * 1000;
-  const [icon, setIcon] = React.useState(getIcon(statusColor, lastSeen));
+  const [icon, setIcon] = React.useState(getIcon(lastSeen));
   const { backgrounds } = useLight();
 
   useInterval(
     () => {
-      setIcon(getIcon(statusColor, lastSeen));
+      setIcon(getIcon(lastSeen));
     },
     onlineNow ? 2000 : 1000 * 60 * 60
   );
