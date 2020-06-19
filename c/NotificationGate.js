92211e6196af53a4ba6ffcb10aae1a8ac7d78e14
Matthew Bunday <mkbunday@gmail.com>
2020-06-19 19:06:22 -0400

    Try really hard to get push notification permissions in a good way

--- c/NotificationGate.js
+++ c/NotificationGate.js
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import { Notifications } from 'expo';
+import { Alert } from 'react-native';
 import Constants from 'expo-constants';
 import * as Permissions from 'expo-permissions';
 
@@ -9,14 +10,24 @@ import SfContainer from '~/c/SfContainer';
 import SfButton from '~/c/SfButton';
 import SfText from '~/c/SfText';
 
+import { FREE } from '~/constants/Colors';
+
 const NotificationAskScreen = (props) => {
   return (
     <SfContainer>
-      <SfText>
-        Supfam uses your status to decide whether to send notifications. Enable
-        notifications to see when your fam messages.
+      <SfText style={{ fontSize: 16, marginBottom: 16, marginTop: 16 }}>
+        We built Supfam in large part because we hate notification spam. Supfam
+        uses your status to decide whether to send notifications. Set your
+        status to Away and we won't interrupt your flow. Enable notifications to
+        see when your fam messages.
       </SfText>
-      <SfButton title="Allow notifications" />
+      <SfButton title="Allow notifications" onPress={props.requestPermission} />
+      <SfButton
+        title="Skip for now"
+        onPress={props.requestPermission}
+        color={FREE}
+        onPress={props.skipForNow}
+      />
     </SfContainer>
   );
 };
@@ -44,12 +55,43 @@ const ProfileGate = (props) => {
     f();
   }, []);
 
+  const requestPermission = async () => {
+    Alert.alert(
+      'Allow Supfam to send notifications?',
+      'At any time U can set your status to Away to turn off all notifications from Supfam. For the best experience we recommend allowing notifications.',
+      [
+        {
+          text: 'Not Now',
+          onPress: () => {
+            setNotificationsEnabled(true);
+          },
+        },
+        {
+          text: 'OK',
+          onPress: async () => {
+            await Permissions.askAsync(Permissions.NOTIFICATIONS);
+          },
+        },
+      ],
+      { cancelable: false }
+    );
+  };
+
+  const skipForNow = () => {
+    setNotificationsEnabled(true);
+  };
+
   if (waiting) {
     return <FullScreenLoader />;
   }
 
   if (!notificationsEnabled) {
-    return <NotificationAskScreen />;
+    return (
+      <NotificationAskScreen
+        requestPermission={requestPermission}
+        skipForNow={skipForNow}
+      />
+    );
   }
 
   return props.children;
