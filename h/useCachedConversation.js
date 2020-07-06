1e787d5c1e04b86d1f19c7d8f1da569c09a5cc9d
Matthew Bunday <mkbunday@gmail.com>
2020-07-06 10:46:17 -0400

    Refactor to useCachedConversation

--- h/useCachedConversation.js
+++ h/useCachedConversation.js
@@ -1,9 +1,11 @@
 import { useQuery } from 'react-query';
 
+import { getConversation } from '~/apis/api';
+
 export default function useCachedProfile(conversationId) {
   const { data: conversation } = useQuery(
     ['conversation', conversationId],
-    () => {},
+    getConversation,
     {
       staleTime: Infinity,
       enabled: conversationId,
