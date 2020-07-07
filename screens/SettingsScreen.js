7084d0bba77f039a7717ae027f748c8e3ea7f5b5
estitesc <evan@teespring.com>
2020-07-07 11:40:07 -0700

    Adds app version number to settings screen

--- screens/SettingsScreen.js
+++ screens/SettingsScreen.js
@@ -2,12 +2,16 @@ import * as React from 'react';
 import AuthToken from '~/lib/AuthToken';
 import { LOGOUT } from '~/apis/auth/actions';
 import { connect } from 'react-redux';
+import { View } from 'react-native';
 
 import SfKeyboardAvoidingView from '~/c/SfKeyboardAvoidingView';
 import SfButton from '~/c/SfButton';
+import SfText from '~/c/SfText';
 import SettingsTopBar from '~/c/SettingsTopBar';
 
+import Constants from 'expo-constants';
 import statusColors from '~/constants/statusColors';
+import {nord10} from '~/constants/Colors';
 
 import downloadUpdate from '~/lib/downloadUpdate';
 
@@ -29,6 +33,22 @@ export default connect()(function LinksScreen(props) {
         onPress={downloadUpdate}
         color={statusColors[2]}
       />
+      <View style={{
+        marginTop: 48,
+        width: '100%',
+        alignItems: 'center',
+      }}>
+        <SfText
+          style={{
+            fontSize: 20,
+            textAlign: 'center',
+            width: '80%',
+            color: nord10,
+          }}
+        >
+          App version {Constants.nativeAppVersion}
+        </SfText>
+      </View>
     </SfKeyboardAvoidingView>
   );
 });
