ab8c5b09d6086edda4473fac4ffda3f81b20319a
Matthew Bunday <mkbunday@gmail.com>
2020-07-08 17:07:21 -0400

    Put 'paused' back

--- c/TypingText.js
+++ c/TypingText.js
@@ -3,16 +3,16 @@ import React, { useState } from 'react';
 import useInterval from '@use-it/interval';
 
 const getDisplay = (time) => {
-  return new Date() - time > 1500 ? '...' : '';
+	return new Date() - time > 1500 ? '...paused' : '';
 };
 
 const TypingText = ({ time }) => {
-  const [timeDisplay, setTimeDisplay] = useState(getDisplay(time));
+	const [timeDisplay, setTimeDisplay] = useState(getDisplay(time));
 
-  useInterval(() => {
-    setTimeDisplay(getDisplay(time));
-  }, 500);
+	useInterval(() => {
+		setTimeDisplay(getDisplay(time));
+	}, 500);
 
-  return <React.Fragment>{timeDisplay}</React.Fragment>;
+	return <React.Fragment>{timeDisplay}</React.Fragment>;
 };
 export default TypingText;
