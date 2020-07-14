4c0fa63dfbc8b9e5a057d707e6e1feebba4b86e9
Matthew Bunday <mkbunday@gmail.com>
2020-07-14 10:28:11 -0400

    Light styling on modal

--- c/AddChoiceModal.js
+++ c/AddChoiceModal.js
@@ -1,19 +1,20 @@
 import * as React from 'react';
 
-import {StyleSheet, Text} from 'react-native';
+import { StyleSheet, Text } from 'react-native';
 
 import SfModal from '~/c/SfModal';
 import SfButton from '~/c/SfButton';
 import SfTextButton from '~/c/SfTextButton';
+import SfText from '~/c/SfText';
 
-export default function AddChoiceModal({visible, setVisible, navigation}) {
+export default function AddChoiceModal({ visible, setVisible, navigation }) {
   return (
     <SfModal visible={visible} setVisible={setVisible}>
       <>
-        <Text style={styles.modalText}>What would you like to do?</Text>
+        <SfText style={styles.modalText}>What would you like to do?</SfText>
 
         <SfButton
-          title='Invite Friend(s)'
+          title="Invite Friends"
           onPress={() => {
             navigation.navigate('Invite');
             setVisible(false);
@@ -23,9 +24,12 @@ export default function AddChoiceModal({visible, setVisible, navigation}) {
             paddingLeft: 24,
             paddingRight: 24,
           }}
+          buttonTextStyle={{
+            fontSize: 14,
+          }}
         />
         <SfButton
-          title='Create Group'
+          title="Create Group"
           onPress={() => {
             navigation.navigate('New Group', { conversationId: null });
             setVisible(false);
@@ -35,12 +39,18 @@ export default function AddChoiceModal({visible, setVisible, navigation}) {
             paddingLeft: 24,
             paddingRight: 24,
           }}
+          buttonTextStyle={{
+            fontSize: 14,
+          }}
         />
         <SfTextButton
-          title='cancel'
+          title="cancel"
           onPress={() => setVisible(false)}
           style={{
             marginTop: 16,
+          }}
+          buttonTextStyle={{
+            fontSize: 14,
           }}
         />
       </>
@@ -51,28 +61,28 @@ export default function AddChoiceModal({visible, setVisible, navigation}) {
 const styles = StyleSheet.create({
   centeredView: {
     flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-    marginTop: 22
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginTop: 22,
   },
   modalView: {
     margin: 16,
-    backgroundColor: "white",
+    backgroundColor: 'white',
     borderRadius: 8,
     padding: 24,
-    alignItems: "center",
-    shadowColor: "#000",
+    alignItems: 'center',
+    shadowColor: '#000',
     shadowOffset: {
       width: 0,
-      height: 2
+      height: 2,
     },
     shadowOpacity: 0.25,
     shadowRadius: 3.84,
-    elevation: 5
+    elevation: 5,
   },
   modalText: {
-    fontSize: 18,
+    fontSize: 16,
     marginBottom: 16,
-    textAlign: "center"
-  }
+    textAlign: 'center',
+  },
 });
--- c/SfModal.js
+++ c/SfModal.js
@@ -1,25 +1,36 @@
 import * as React from 'react';
 
-import {Alert, Modal, StyleSheet, View, TouchableWithoutFeedback, TouchableOpacity} from 'react-native';
+import {
+  Alert,
+  Modal,
+  StyleSheet,
+  View,
+  TouchableWithoutFeedback,
+  TouchableOpacity,
+} from 'react-native';
 
-export default function SfModal({visible, setVisible, children}) {
+import useLight from '~/h/useLight';
+
+export default function SfModal({ visible, setVisible, children }) {
+  const { backgrounds } = useLight();
+
   return (
     <Modal
       animationType="slide"
       transparent={true}
       visible={visible}
       onRequestClose={() => {
-        Alert.alert("Modal has been closed.");
+        Alert.alert('Modal has been closed.');
         setVisible(false);
       }}
     >
-      <TouchableOpacity 
-        style={styles.centeredView} 
-        activeOpacity={1} 
+      <TouchableOpacity
+        style={styles.centeredView}
+        activeOpacity={1}
         onPressOut={() => setVisible(false)}
       >
         <TouchableWithoutFeedback>
-          <View style={styles.modalView}>
+          <View style={[styles.modalView, { backgroundColor: backgrounds[0] }]}>
             {children}
           </View>
         </TouchableWithoutFeedback>
@@ -31,28 +42,29 @@ export default function SfModal({visible, setVisible, children}) {
 const styles = StyleSheet.create({
   centeredView: {
     flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-    marginTop: 22
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginTop: 22,
+    backgroundColor: 'rgba(0, 0, 0, 0.5)',
   },
   modalView: {
     margin: 16,
-    backgroundColor: "white",
+    backgroundColor: 'white',
     borderRadius: 8,
     padding: 24,
-    alignItems: "center",
-    shadowColor: "#000",
+    alignItems: 'center',
+    shadowColor: '#000',
     shadowOffset: {
       width: 0,
-      height: 2
+      height: 2,
     },
     shadowOpacity: 0.25,
     shadowRadius: 3.84,
-    elevation: 5
+    elevation: 5,
   },
   modalText: {
     fontSize: 18,
     marginBottom: 16,
-    textAlign: "center"
-  }
+    textAlign: 'center',
+  },
 });
