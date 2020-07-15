8ccdcf206ea93e7748018e61ae80108a21c72fe4
Matthew Bunday <mkbunday@gmail.com>
2020-07-15 17:44:41 -0400

    Move to ts

---
+++ c/ThemeAwareStatusBar.tsx
@@ -0,0 +1,17 @@
+import * as React from 'react';
+import { useColorScheme } from 'react-native-appearance';
+import { StatusBar } from 'react-native';
+
+import useLight from '~/h/useLight';
+
+export default function ThemeAwareStatusBar() {
+	const colorScheme = useColorScheme();
+	const { backgrounds } = useLight();
+
+	return (
+		<StatusBar
+			barStyle={colorScheme === 'light' ? 'dark-content' : 'light-content'}
+			backgroundColor={backgrounds[0]}
+		/>
+	);
+}
