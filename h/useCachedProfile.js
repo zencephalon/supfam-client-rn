7495208f08d1e6ff7061c7bd18c82c736e0c1f62
Matthew Bunday <mkbunday@gmail.com>
2020-07-21 18:06:19 -0400

    Remove hack

--- h/useCachedProfile.js
+++ h/useCachedProfile.js
@@ -4,22 +4,12 @@ import { useQuery } from 'react-query';
 import { getProfile } from '~/apis/api';
 
 export default function useCachedProfile(profileId) {
-	const [profile, setProfile] = React.useState(null);
 	const { data: _profile } = useQuery(['friend', profileId], getProfile, {
 		// staleTime: Infinity,
 		// manual: true,
 		// staleTime: 60 * 1000,
 		enabled: profileId,
 	});
-
-	// Stupid hack, some React.memo components won't know to update
-	// unless we make it obvious for them using a useState
-	// In theory useContext should take care of it, but it seems
-	// like useQuery's useContext does special caching magic that
-	// makes this not update the component as normal
-	React.useEffect(() => {
-		setProfile(_profile);
-	}, [_profile]);
 
 	return _profile;
 }
