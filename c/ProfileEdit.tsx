bd36db8a0e7f80759e3f6d42079fd047afd0b041
Matthew Bunday <mkbunday@gmail.com>
2020-07-22 17:50:56 -0400

    Simplify profile creation flow

--- c/ProfileEdit.tsx
+++ c/ProfileEdit.tsx
@@ -4,15 +4,23 @@ import SfContainer from '~/c/SfContainer';
 import SfText from '~/c/SfText';
 import SfTextInput from '~/c/SfTextInput';
 import SfButton from '~/c/SfButton';
+import { BareProfileIcon } from '~/c/ProfileIcon';
+
+import { FREE } from '~/constants/Colors';
 
 import { uploadImage, postProfile } from '~/apis/api';
 
 import { queryCache } from 'react-query';
 
 import useApi from '~/h/useApi';
+import useSnapImage from '~/h/useSnapImage';
+import usePickImage from '~/h/usePickImage';
+
+// Stubbed for editing
 
 function ProfileCreate() {
   const [name, setName] = React.useState('');
+  const [image, setImage] = React.useState(null);
   const PostProfile = useApi(postProfile, {
     onConfirm: () => {
       queryCache.invalidateQueries('profilesMe');
@@ -20,14 +28,25 @@ function ProfileCreate() {
   });
   const UploadImage = useApi(uploadImage);
 
+  const snapImage = useSnapImage({
+    setImage,
+    config: { allowsEditing: true, aspect: [1, 1] },
+  });
+  const pickImage = usePickImage({
+    setImage,
+    imageOptions: { allowsEditing: true, aspect: [1, 1] },
+  });
+
   const profileBusy = PostProfile.req.requested || UploadImage.req.requested;
-  const cantCreateProfile = !name;
+  const cantCreateProfile = !name || !image;
   const createProfile = async () => {
     if (profileBusy || cantCreateProfile) {
       return;
     }
 
-    return PostProfile.call({ name });
+    const { key: avatar_key } = await UploadImage.call(image.uri);
+    // await postProfile({ name, avatar_url });
+    return PostProfile.call({ name, avatar_key });
   };
 
   return (
@@ -41,6 +60,16 @@ function ProfileCreate() {
         ok={!!name}
         autoCapitalize="words"
       />
+      <SfText style={styles.formLabel}>Set profile photo from:</SfText>
+      <SfButton round title="Camera" onPress={snapImage} color={FREE} />
+      <SfButton round title="Camera roll" onPress={pickImage} color={FREE} />
+      {!!image && (
+        <BareProfileIcon
+          uri={image.uri}
+          size={100}
+          style={{ alignSelf: 'center' }}
+        />
+      )}
       <SfButton
         round
         wide
