cd4d7f5af4777e23aca80aee67bba4efd5cbc65b
Matthew Bunday <mkbunday@gmail.com>
2020-09-03 14:02:51 -0400

    Refactor useConversation

--- h/useConversation.tsx
+++ h/useConversation.tsx
@@ -11,19 +11,11 @@ const DEFAULT_CONVERSATION_STATE: ConversationState = {
 	latestSyncMessageId: undefined,
 };
 
-export default function useConversation(
+function useStateFromStore(
 	conversationId: number,
-	meProfileId: number
+	setConversationState: React.Dispatch<ConversationState>,
+	setLoadedFromStore: React.Dispatch<boolean>
 ) {
-	// subscribe to conversation
-
-	const [loadedFromStore, setLoadedFromStore] = React.useState(false);
-	const [conversationState, setConversationState] = React.useState(
-		DEFAULT_CONVERSATION_STATE
-	);
-
-	console.log({ conversationId, loadedFromStore });
-
 	React.useEffect(() => {
 		if (!conversationId) {
 			return;
@@ -39,40 +31,79 @@ export default function useConversation(
 				setLoadedFromStore(true);
 			});
 	}, [conversationId]);
+}
 
+function useStoreState(
+	conversationId: number,
+	loadedFromStore: boolean,
+	conversationState: ConversationState
+) {
 	React.useEffect(() => {
 		if (!conversationId || !loadedFromStore) {
 			return;
 		}
 		storeConversation(conversationId, conversationState);
 	}, [conversationState]);
+}
 
-	const sync = React.useCallback(() => {
+function useSync(
+	conversationId: number,
+	loadedFromStore: boolean,
+	latestSyncMessageId: number,
+	setConversationState: React.Dispatch<React.SetStateAction<ConversationState>>
+) {
+	return React.useCallback((): void => {
 		if (!conversationId || !loadedFromStore) {
 			return;
 		}
 		// make API call to get message previous to now, merge into messages
-		getConversationMessagesSync(
-			conversationId,
-			conversationState.latestSyncMessageId
-		).then((_messages: Message[]) => {
-			// merge into messages
-			setConversationState((conversationState) => {
-				const messages = mergeSortedIds(conversationState.messages, _messages);
-				const state = {
-					...conversationState,
-					messages,
-					latestSyncMessageId: messages?.[0]?.id,
-				};
-				return state;
-			});
-		});
+		getConversationMessagesSync(conversationId, latestSyncMessageId).then(
+			(_messages: Message[]) => {
+				// merge into messages
+				setConversationState((conversationState) => {
+					const messages = mergeSortedIds(
+						conversationState.messages,
+						_messages
+					);
+					const state = {
+						...conversationState,
+						messages,
+						latestSyncMessageId: messages?.[0]?.id,
+					};
+					return state;
+				});
+			}
+		);
 	}, [
 		loadedFromStore,
 		conversationId,
-		conversationState.latestSyncMessageId,
+		latestSyncMessageId,
 		setConversationState,
 	]);
+}
+
+export default function useConversation(
+	conversationId: number,
+	meProfileId: number
+) {
+	// subscribe to conversation
+
+	const [loadedFromStore, setLoadedFromStore] = React.useState(false);
+	const [conversationState, setConversationState] = React.useState(
+		DEFAULT_CONVERSATION_STATE
+	);
+
+	console.log({ conversationId, loadedFromStore });
+
+	useStateFromStore(conversationId, setConversationState, setLoadedFromStore);
+	useStoreState(conversationId, loadedFromStore, conversationState);
+
+	const sync = useSync(
+		conversationId,
+		loadedFromStore,
+		conversationState.latestSyncMessageId,
+		setConversationState
+	);
 
 	React.useEffect(sync, [loadedFromStore]);
 
