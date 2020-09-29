9fc171b47f3aecd04a77cee751f3c97714c51926
Matthew Bunday <mkbunday@gmail.com>
2020-09-29 14:50:31 -0400

    Clean up ChatScreen

--- screens/ChatScreen.js
+++ screens/ChatScreen.js
@@ -1,16 +1,7 @@
 import * as React from 'react';
 
-import MessageList from '~/c/MessageList';
-import MessageInput from '~/c/MessageInput';
-import SfKeyboardAvoidingView from '~/c/SfKeyboardAvoidingView';
 import GroupConversationTopBar from '~/c/GroupConversationTopBar';
-import EmptyListPlaceholder from '~/c/EmptyListPlaceholder';
 import MessagingInterface from '~/c/MessagingInterface';
-
-import useProfileId from '~/h/useProfileId';
-import useMarkConversationRead from '~/h/useMarkConversationRead';
-import useConversationSelect from '~/h/useConversationSelect';
-import useConversation from '~/h/useConversation';
 
 const placeholderText =
   'No messages have been sent in this group yet. Be the first!';
